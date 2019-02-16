5a55447156de450e31f783bbdd3f1d6159a0a594
Vaibhav Darji <40035566+VJDarji@users.noreply.github.com>
2019-02-16 17:30:02 -0500

    Open and close the file for reading

--- index.php
+++ index.php
@@ -14,5 +14,17 @@ if (($h = fopen("project1.csv", "r")) !== FALSE) {
 // The nested array to hold all the arrays
     $the_big_array = [];
 
+    // Open the file for reading
+    if (($h = fopen("{$filename}", "r")) !== FALSE) {
+        // Each line in the file is converted into an individual array that we call $data
+        // The items of the array are comma separated
+        while (($data = fgetcsv($h, 1000, ",")) !== FALSE) {
+            // Each individual array is being pushed into the nested array
+            $the_big_array[] = $data;
+        }
+
+        // Close the file
+        fclose($h);
+    }
 
 
